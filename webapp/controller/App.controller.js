e36fbb9a163e160dcf8fcd532b7749594c6e2827
rspseshasai <rspseshasai@gmail.com>
2025-05-14 00:49:32 +0530

    Step 15: Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/core/mvc/Controller"
 ],
-    (Controller, MessageToast, JSONModel, ResourceModel) => {
+    (Controller) => {
         "use strict";
-        return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
-            onShowHello: function () {
-
-                //read the message from i18n model
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMessage", [sRecipient]);
-
-                MessageToast.show(sMsg);
-            },
+        return Controller.extend("sap.ui.demo.walkthrough.App", {
         });
     });
---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], (Controller, MessageToast) => {
+   "use strict";
+
+   return Controller.extend("sap.ui5.demo.walkthrough.controller.HelloPanel", {
+      onShowHello() {
+         // read msg from i18n model
+         const oBundle = this.getView().getModel("i18n").getResourceBundle();
+         const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+         const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+         // show message
+         MessageToast.show(sMsg);
+      }
+   });
+});
