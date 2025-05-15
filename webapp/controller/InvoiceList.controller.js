4d0822302a386eeb4e72473aadb503799cc8ecf6
rspseshasai <rspseshasai@gmail.com>
2025-05-16 01:53:08 +0530

    Step 22: Custom Formatters

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+	"sap/ui/model/json/JSONModel",
+    "../model/formatter"
+], (Controller, JSONModel, formatter) => {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
+        formatter: formatter,
 		onInit() {
 			var oViewModel = new JSONModel({
 				currency: "EUR"
